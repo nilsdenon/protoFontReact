7e90bbb2d90b74e6a4229f287fbf0b81eb7d1b74
Damien Pierre <kalumbox@gmail.com>
2019-03-25 15:46:03 +0100

    duplicatable fontblocks

--- src/components/IncorporationForm/FontBlock.jsx
+++ src/components/IncorporationForm/FontBlock.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './IncorporationForm.scss';
+import './FontBlock.scss';
 
 export default class FontBlock extends React.Component {
     constructor() {
@@ -42,7 +42,10 @@ export default class FontBlock extends React.Component {
     handleAddFontblock = () => {
         this.setState({
             blocks: this.state.blocks.concat([
-                { primaryText: '', secondaryText: '' }
+                {
+                    primaryText: this.state.primaryText,
+                    secondaryText: this.state.secondaryText
+                }
             ])
         });
     };
@@ -57,26 +60,98 @@ export default class FontBlock extends React.Component {
         return (
             <div>
                 {this.state.blocks.map((fontblock, idx) => (
-                    <div key={idx} className="fontBlock">
-                        <textarea
-                            placeholder={`Primary Text #${idx + 1}`}
-                            onChange={this.handlePrimaryFontblockNameChange(
-                                idx
-                            )}
-                        />
-                        <div className="text">
-                            <p>{fontblock.primaryText}</p>
-                        </div>
+                    <div key={idx} className="fontblock is-open">
+                        <section className="fontblock__body">
+                            <div className="fontblock__headline">
+                                <div className="font-container">
+                                    <div className="font font-primary">
+                                        {fontblock.primaryText}
+                                    </div>
+                                    <dl className="hud hud--fontdetails">
+                                        <dt>Font Family</dt>
+                                        <dd className="hud-fontName">test</dd>
+                                        <dt>Font Size</dt>
+                                        <dd className="hud-fontSize">test</dd>
+                                        <dt>Letter-Spacing</dt>
+                                        <dd className="hud-letterSpacing">
+                                            test
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                            <div className="fontblock__bodytext">
+                                <div className="font-container">
+                                    <div
+                                        id="font_1_2"
+                                        className="font font-secondary"
+                                    >
+                                        {fontblock.secondaryText}
+                                    </div>
+                                    <dl
+                                        id="hud_1_2"
+                                        className="hud_1_2 hud hud--fontdetails"
+                                    >
+                                        <dt>Font Family</dt>
+                                        <dd className="hud-fontName">test</dd>
+                                        <dt>Font Size</dt>
+                                        <dd className="hud-fontSize">test</dd>
+                                        <dt>Letter-Spacing</dt>
+                                        <dd className="hud-letterSpacing">
+                                            test
+                                        </dd>
+                                    </dl>
+                                </div>
+                            </div>
+                        </section>
+                        <aside className="fontblock__menu">
+                            <nav className="fontblock__nav">
+                                <form
+                                    name="fontblock_form_1"
+                                    className="fontblock__form"
+                                    action=""
+                                >
+                                    <fieldset
+                                        className="sep"
+                                        id="subform__headline"
+                                    >
+                                        <legend>Headline</legend>
+                                        <div className="form-group">
+                                            <textarea
+                                                className="custom-textarea"
+                                                placeholder={`Primary Text #${idx +
+                                                    1}`}
+                                                onChange={this.handlePrimaryFontblockNameChange(
+                                                    idx
+                                                )}
+                                                defaultValue={
+                                                    fontblock.primaryText
+                                                }
+                                            />
+                                        </div>
+                                    </fieldset>
+                                    <fieldset
+                                        className="sep"
+                                        id="subform__bodytext"
+                                    >
+                                        <legend>Body Text</legend>
+                                        <div className="form-group">
+                                            <textarea
+                                                className="custom-textarea"
+                                                placeholder={`Secondary Text #${idx +
+                                                    2}`}
+                                                onChange={this.handleSecondaryFontblockNameChange(
+                                                    idx
+                                                )}
+                                                defaultValue={
+                                                    fontblock.secondaryText
+                                                }
+                                            />
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </nav>
+                        </aside>
 
-                        <textarea
-                            placeholder={`Secondary Text #${idx + 2}`}
-                            onChange={this.handleSecondaryFontblockNameChange(
-                                idx
-                            )}
-                        />
-                        <div className="text">
-                            <p>{fontblock.secondaryText}</p>
-                        </div>
                         <button
                             type="button"
                             onClick={this.handleRemoveFontblock(idx)}
